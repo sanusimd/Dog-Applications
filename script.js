26753f6d18b8e66296457253c53ddf855e806811
Sanusi Muhyideen <just4deen@yahoo.com>
2022-06-08 16:43:07 +0100

    Create functionality to fetch breeds and lists

---
+++ script.js
@@ -0,0 +1,28 @@
+// fetch the breeds list
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  //   console.log(data);
+  createBreedLists(data.message);
+}
+
+start();
+// Create Breed Lists
+function createBreedLists(breedList) {
+  document.getElementById("breed").innerHTML = ` 
+    <select onChange = 'loadByBreed(this.value)'>
+    <option>Choose a Dog Bread</option>
+     ${Object.keys(breedList).map(function (breed) {
+       return `<Option>${breed}</Option>`;
+     })}
+  </select>`;
+}
+
+// Function to load breed
+async function loadByBreed(breed) {
+  if (breed != "Choose a Dog Bread") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await response.json();
+    console.log(data);
+  }
+}
