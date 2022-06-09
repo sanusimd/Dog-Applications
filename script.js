aae73af8049adebe887e4b362e28abb5ca944fc3
Sanusi Muhyideen <just4deen@yahoo.com>
2022-06-09 13:31:18 +0100

    create functionality for slideshow

--- script.js
+++ script.js
@@ -23,6 +23,18 @@ async function loadByBreed(breed) {
   if (breed != "Choose a Dog Bread") {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
-    console.log(data);
+    createSlideshow(data.message);
   }
+}
+
+// Create  Slideshow function
+function createSlideshow(images) {
+  document.getElementById("slideshow").innerHTML = `
+    <div
+    class="slide"
+    style="
+      background-image: url('${images[0]}');
+    "
+  ></div>
+   `;
 }
